29e65358d40ad3b4565bf8344aa2bce879018ae9
Anshul Dahiya <anshul.rts@gmail.com>
2020-05-01 13:09:37 +0530

    Getting Result from Database - Now that we have setup the DbContext in previous step, we can use it in our controller. When we set up the AppDbContext in configure(), it added the Scoped instance of the context automatically. - To check - If we add AppDbContextPool, we might have to add either scoped/singleton/transient separately - In asp.net core, we return IActionResult or ActionResult. There is no IHttpActionResult in it

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +12,27 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
-        public string GetProducts()
+        public ActionResult<List<Product>> GetProducts()
         {
-            return "this will be a list of product";
+            var products = _context.Products.ToList();
+
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public ActionResult<Product> GetProduct(int id)
         {
-            return "this will be a list of product";
+            var product = _context.Products.Find(id);
+
+            return Ok(product);
         }
     }
 }
--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -14,6 +14,6 @@ namespace API.Data
 
         }
 
-        DbSet<Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
