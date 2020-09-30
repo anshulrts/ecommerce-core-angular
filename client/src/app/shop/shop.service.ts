1e1d511329805214ea7631735dd7d1970298507c
Anshul Dahiya <anshul.rts@gmail.com>
2020-09-30 12:27:29 +0530

    Moved API call to Service and Import/Export Component - Officially, all API calls must be given in service and not in the component directly. - There are 2 different modules here, App and Shop. In order to use the component defined in Shop in App module, we export it from ShopModule and import the module in Appi

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IPagination } from '../shared/models/pagination';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ShopService {
+export class ShopService implements OnInit{
+  baseUrl = "https://localhost:44382/api/"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  }
 }
